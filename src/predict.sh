cd3c6043d0391df72f621f084aedb9924324fb8d
akiFQC <yakaredori@gmail.com>
2021-08-22 19:36:03 +0900

    update

--- src/predict.sh
+++ src/predict.sh
@@ -1,4 +1,19 @@
 python predict.py \
-    --input_path /home/afukuchi/Codes/shinra-attribute-extraction/2020jp_tokenize_mecab_ipadic_bpe_tohoku_bert/JP-5/data1/ujiie/shinra/tohoku_bert/JP-5/Company/ \
+    --input_path /home/afukuchi/Codes/shinra-attribute-extraction/for_leaderbord_JP-5/inputs/tohoku_bert/JP-5/City \
+    --model_path /home/afukuchi/Codes/shinra-attribute-extraction/models/City/best.model \
+    --output_path /home/afukuchi/Codes/shinra-attribute-extraction/for_leaderbord_JP-5/outputs/tohoku_bert/JP-5/Submission/City.json \
+    --plane_path /home/afukuchi/Codes/shinra-attribute-extraction/test_for_leaderboard/target_data/plain/City ;  \
+
+echo "city end";
+# 空行を除いて上書き保存
+echo "空行削除"
+sed -i '/^$/d' /home/afukuchi/Codes/shinra-attribute-extraction/for_leaderbord_JP-5/outputs/tohoku_bert/JP-5/Submission/City.json
+
+python predict.py \
+    --input_path /home/afukuchi/Codes/shinra-attribute-extraction/for_leaderbord_JP-5/inputs/tohoku_bert/JP-5/Company \
     --model_path /home/afukuchi/Codes/shinra-attribute-extraction/models/Company/best.model \
-    --output_path /home/afukuchi/Codes/shinra-attribute-extraction/predictions/Company/prediction_20210818.json
+    --output_path /home/afukuchi/Codes/shinra-attribute-extraction/for_leaderbord_JP-5/outputs/tohoku_bert/JP-5/Submission/Company.json \
+    --plane_path /home/afukuchi/Codes/shinra-attribute-extraction/test_for_leaderboard/target_data/plain/Company ;\
+echo "空行削除"
+# 空行を除いて上書き保存
+sed -i '/^$/d' /home/afukuchi/Codes/shinra-attribute-extraction/for_leaderbord_JP-5/outputs/tohoku_bert/JP-5/Submission/Company.json
